f7b98b11dfc6bae927910b7da1ec580259ad81a7
santhosh <santhoshmuruganandham023@gmail.com>
2025-10-03 23:30:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PowerGridForm from './PowerGridForm'
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <PowerGridForm />
+    </div>
+  )
+}
